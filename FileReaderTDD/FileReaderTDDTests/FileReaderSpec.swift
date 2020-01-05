50c228585bac5dca1c6766901a90083fed284eb5
Jakub Turek <jkbturek@gmail.com>
2020-01-05 18:13:14 +0100

    Add a failing test for fetching documents URL

--- FileReaderTDD/FileReaderTDDTests/FileReaderSpec.swift
+++ FileReaderTDD/FileReaderTDDTests/FileReaderSpec.swift
@@ -10,6 +10,7 @@ func fileReader(path: String) -> String? {
 class FileReaderSpec: QuickSpec {
     override func spec() {
         describe("fileReader") {
+            var documentsURL: String?
             var fileContents: String?
 
             beforeEach {
@@ -18,6 +19,10 @@ class FileReaderSpec: QuickSpec {
 
             afterEach {
                 fileContents = nil
+            }
+
+            it("should return documents URL") {
+                expect(documentsURL).toNot(beNil())
             }
 
             it("should return content read from a file") {

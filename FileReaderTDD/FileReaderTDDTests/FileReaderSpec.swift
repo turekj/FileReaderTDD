c8fd7b782ca042b48b9be46c4fa0111bf933112c
Jakub Turek <jkbturek@gmail.com>
2020-01-05 18:55:27 +0100

    Reafactor FileReader to be a class, not a method

--- FileReaderTDD/FileReaderTDDTests/FileReaderSpec.swift
+++ FileReaderTDD/FileReaderTDDTests/FileReaderSpec.swift
@@ -2,38 +2,52 @@ import Nimble
 import Quick
 @testable import FileReaderTDD
 
-func fileReader(path: String,
-                fileManager: FileManaging = FileManager.default,
-                urlReader: (URL) throws -> String = String.init(contentsOf:)) throws -> String {
-    guard let documentsURL = fileManager.documentsURL?.appendingPathComponent(path) else {
-        fatalError()
-    }
+class FileReader {
+    func read(path: String,
+              fileManager: FileManaging = FileManager.default,
+              urlReader: (URL) throws -> String = String.init(contentsOf:)) throws -> String {
+        guard let documentsURL = fileManager.documentsURL?.appendingPathComponent(path) else {
+            fatalError()
+        }
 
-    return try urlReader(documentsURL)
+        return try urlReader(documentsURL)
+    }
 }
 
 class FileReaderSpec: QuickSpec {
     override func spec() {
-        describe("fileReader") {
-            var fileURL: URL!
-            var fileContents: String?
+        describe("FileReader") {
+            var sut: FileReader!
 
             beforeEach {
-                fileURL = FileManager.default.documentsURL?.appendingPathComponent("a-021-293-121-test.txt")
-                try! "FILE CONTENTS".write(to: fileURL, atomically: true, encoding: .utf8)
-
-                fileContents = try? fileReader(path: "a-021-293-121-test.txt")
+                sut = FileReader()
             }
 
             afterEach {
-                try! FileManager.default.removeItem(at: fileURL)
-                fileURL = nil
-                fileContents = nil
+                sut = nil
             }
 
-            it("should return content read from a file") {
-                expect(fileContents).toNot(beNil())
-                expect(fileContents) == "FILE CONTENTS"
+            describe("read") {
+                var fileURL: URL!
+                var fileContents: String?
+
+                beforeEach {
+                    fileURL = FileManager.default.documentsURL?.appendingPathComponent("a-021-293-121-test.txt")
+                    try! "FILE CONTENTS".write(to: fileURL, atomically: true, encoding: .utf8)
+
+                    fileContents = try? sut.read(path: "a-021-293-121-test.txt")
+                }
+
+                afterEach {
+                    try! FileManager.default.removeItem(at: fileURL)
+                    fileURL = nil
+                    fileContents = nil
+                }
+
+                it("should return content read from a file") {
+                    expect(fileContents).toNot(beNil())
+                    expect(fileContents) == "FILE CONTENTS"
+                }
             }
         }
     }

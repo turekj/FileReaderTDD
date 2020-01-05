c7113c170558203191b2a611372a457cff3e206c
Jakub Turek <jkbturek@gmail.com>
2020-01-05 18:08:00 +0100

    Add a failing test for the file reader

--- FileReaderTDD/FileReaderTDDTests/FileReaderSpec.swift
+++ FileReaderTDD/FileReaderTDDTests/FileReaderSpec.swift
@@ -3,8 +3,26 @@ import Quick
 import XCTest
 @testable import FileReaderTDD
 
+func fileReader(path: String) -> String? {
+    return nil
+}
+
 class FileReaderSpec: QuickSpec {
     override func spec() {
+        describe("fileReader") {
+            var fileContents: String?
 
+            beforeEach {
+                fileContents = fileReader(path: "a-021-293-121-test.txt")
+            }
+
+            afterEach {
+                fileContents = nil
+            }
+
+            it("should return content read from a file") {
+                expect(fileContents).toNot(beNil())
+            }
+        }
     }
 }

d4ec0d55476c26bfcce610b2d54493adf4119f49
Jakub Turek <jkbturek@gmail.com>
2020-01-05 19:23:13 +0100

    Make the code throw an actual error when the file is missing

--- FileReaderTDD/FileReaderTDDTests/Stubs/URLReaderStubFactory.swift
+++ FileReaderTDD/FileReaderTDDTests/Stubs/URLReaderStubFactory.swift
@@ -4,7 +4,7 @@ class URLReaderStubFactory {
     static func make(_ files: [String: String]) -> (URL) throws -> String {
         return { url in
             guard let fileContents = files[url.absoluteString] else {
-                fatalError()
+                throw NSError(domain: "URLReaderError", code: 404, userInfo: nil)
             }
 
             return fileContents

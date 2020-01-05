f9d7f6905e962a944bdcdbfb8bc1e1ff7266b3a3
Jakub Turek <jkbturek@gmail.com>
2020-01-05 19:32:41 +0100

    Add a failing test case for dropPrefix test case

--- FileReaderTDD/FileReaderTDDTests/DropPrefixSpec.swift
+++ FileReaderTDD/FileReaderTDDTests/DropPrefixSpec.swift
@@ -11,6 +11,10 @@ class DropPrefixSpec: QuickSpec {
             it("should drop prefix if there's one") {
                 expect(dropPrefix("prefix", from: "prefix_string")) == "_string"
             }
+
+            it("should NOT drop prefix if there's none") {
+                expect(dropPrefix("not_a_prefix", from: "some_string")) == "some_string"
+            }
         }
     }
 }

0b1e8ab3a58950050c9b7f09fd7382bbfac543dd
Jakub Turek <jkbturek@gmail.com>
2020-01-05 17:52:04 +0100

    Add empty iOS project

---
+++ FileReaderTDD/FileReaderTDD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FileReaderTDD
+//
+//  Created by Jakub Turek on 05/01/2020.
+//  Copyright © 2020 Jakub Turek Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
